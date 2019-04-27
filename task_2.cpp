bf6bf793a1b7ff44021d9aacc82ca7afb1fbfc11
Evt-Nadya <evtuhova20vek@gmail.com>
2019-04-27 13:06:11 +0300

    second task

---
+++ task_2.cpp
@@ -0,0 +1,44 @@
+﻿// task_2.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include "pch.h"
+#include <iostream>
+#include <string>
+
+using namespace std;
+class Children {
+private:
+string name, surname;
+	int age;
+public:
+	Children()
+	{
+		name = "temp";
+		surname = "temp";
+		age = 0;
+	}
+	void setData()
+	{
+		cout << "enter children name" << endl;
+		cin >> name;
+		cout << "enter children surname" << endl;
+		cin >> surname;
+		cout << "enter children age" << endl;
+		cin >> age;
+	}
+	void getData()
+	{
+		cout << "name of children: " << name << endl;
+		cout << "surname of children: " << surname << endl;
+		cout << "age of children: " << age << endl;
+	}
+};
+int main()
+{
+	Children first;
+	first.setData();
+	first.getData();
+	Children second;
+	second.setData();
+	second.getData();
+}
